9fb23435b5d600505573d552882d40a78e6d6783
Norberttony <norberttony@icloud.com>
2025-04-19 14:31:42 -0400

    attempting to apply schema to db

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE webapp_db;
+
+CREATE TABLE Posts (
+    id INT PRIMARY KEY,
+    title VARCHAR(50),
+    content TEXT
+);
+
+INSERT INTO Posts (title, content)
+VALUES ('Example Title', 'Example Content');
+
+INSERT INTO Posts (title, content)
+VALUES ('Yet Another Post', 'This is the content of the post, which should be displayed below a title.');
